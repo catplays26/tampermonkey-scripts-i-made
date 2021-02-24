bc532124b7d2736894ef3aa0a477953b88a6c2af
catplays26 <48035783+catplays26@users.noreply.github.com>
2021-02-24 13:40:46 -0500

    Create garfield_scraper_gocomic_newtab.js

---
+++ garfield_scraper_gocomic_newtab.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         GoComic Scraper for Garfield
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Scrape GoComic.
+// @author       winterfoxx
+// @include     https://www.gocomics.com/*
+// @match      https://www.gocomics.com/*
+
+// @run-at document-start
+// ==/UserScript==
+
+(
+    async function() {
+    'use strict';
+
+
+setTimeout(() => {
+    window.location.assign('https://www.gocomics.com/random/garfield');
+
+   var htmlElements = document.getElementsByClassName("img-fluid lazyloaded");
+console.log(htmlElements.length);
+    var img = htmlElements[0];
+    console.log(img.src);
+   window.open(img.src, '_blank');
+
+
+
+}, 5000);
+
+
+
+})();
